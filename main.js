a469a5bbe3c4c10f7f322db27396cb87a379d667
keilsmit <keilsmit@yahoo.com>
2016-11-07 15:43:58 -0500

    working on adventure mode

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 movies = []
-movies.push({ title: "Revolver", director: "Guy Richie", genre: "Thriller", year: "2005", actors: ["Jason Statham", "Ray Liotta", "André Benjamin"], stars: "", metascore: ""})
+movies.push({ title: "Revolver", director: "Guy Richie", genre: "Thriller", year: "2005", actors: ["Jason Statham", "Ray Liotta", "André Benjamin"], stars: "6.5", metascore: "25"})
 
-movies.push({ title: "Crimson Tide", director: "Tony Scott", genre: "Thriller", year: "1993", actors: ["Denzel Washington", "Gene Hackman", "James Gandolfini"]})
+movies.push({ title: "Crimson Tide", director: "Tony Scott", genre: "Thriller", year: "1993", actors: ["Denzel Washington", "Gene Hackman", "James Gandolfini"], stars: "7.3", metascore: "66"})
 
-movies.push({ title: "The Dark Night", director: "Chris Nolan", genre: "Action", year: "2008", actors: ["Christian Bale", "Heath Ledger", "Morgan Freeman"]})
+movies.push({ title: "The Dark Night", director: "Chris Nolan", genre: "Action", year: "2008", actors: ["Christian Bale", "Heath Ledger", "Morgan Freeman"], stars: "9.0", metascore: "82"})
 
-movies.push({ title: "Step Brothers", director: "Adam McCay", genre: "Comedy", year: "2008", actors: ["Will Ferrell", "John C. Reilly", "Mary Steenburgen"]})
+movies.push({ title: "Step Brothers", director: "Adam McCay", genre: "Comedy", year: "2008", actors: ["Will Ferrell", "John C. Reilly", "Mary Steenburgen"], stars: "6.9", metascore: "52"})
 
-movies.push({ title: "City of God", director: "Fernando Meirelles", genre: "Crime", year: "2002", actors: ["Alexandre Rodrigues", "Leandro Firmino", "Phellipe Haagensen"]})
+movies.push({ title: "City of God", director: "Fernando Meirelles", genre: "Crime", year: "2002", actors: ["Alexandre Rodrigues", "Leandro Firmino", "Phellipe Haagensen"], stars: "8.7", metascore: "79"})
 
 
 movie_titles = movies.sort(function(film1 , film2) {
