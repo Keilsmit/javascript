2fe43ae1fbf0c06eefc09e627783625f28028f54
keilsmit <keilsmit@yahoo.com>
2016-11-07 16:49:11 -0500

    used javascript to fill table in html_adventure mode

--- main.js
+++ main.js
@@ -17,4 +17,19 @@ movie_titles = movies.sort(function(film1 , film2) {
 })
 
 
+
+movie_titles.forEach(function(x){
+  my_table_array = ""
+    my_table_array += "<tr><td>" + x.title + "</td>"
+    my_table_array += "<td>" + x.director + "</td>"
+    my_table_array += "<td>" + x.genre + "</td>"
+    my_table_array += "<td>" + x.year + "</td>"
+    my_table_array += "<td>" + x.actors + "</td>"
+    my_table_array += "<td>" + x.stars + "</td>"
+    my_table_array += "<td>" + x.metascore + "</td></tr>"
+
+document.getElementById('movie_data').innerHTML += my_table_array
+})
+
+
 console.log(JSON.stringify(movie_titles))
