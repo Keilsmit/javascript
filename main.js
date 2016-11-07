a0a7b10a1ec9694053fdb040ced50c9915dd64e7
keilsmit <keilsmit@yahoo.com>
2016-11-07 14:48:12 -0500

    explorer mode revised

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 movies = []
-
-movies.push({ title: "Revolver", director: "Guy Richie", genre: "Thriller", year: "2005", actors: ["Jason Statham", "Ray Liotta", "André Benjamin"]})
+movies.push({ title: "Revolver", director: "Guy Richie", genre: "Thriller", year: "2005", actors: ["Jason Statham", "Ray Liotta", "André Benjamin"], stars: "", metascore: ""})
 
 movies.push({ title: "Crimson Tide", director: "Tony Scott", genre: "Thriller", year: "1993", actors: ["Denzel Washington", "Gene Hackman", "James Gandolfini"]})
 
@@ -11,10 +10,11 @@ movies.push({ title: "Step Brothers", director: "Adam McCay", genre: "Comedy", y
 movies.push({ title: "City of God", director: "Fernando Meirelles", genre: "Crime", year: "2002", actors: ["Alexandre Rodrigues", "Leandro Firmino", "Phellipe Haagensen"]})
 
 
-
-movie_titles = movies.sort(function(film)) {
-  return movies.title
-}
+movie_titles = movies.sort(function(film1 , film2) {
+  if(film1.title < film2.title) return -1
+  if(film1.title > film2.title) return 1
+  return 0
+})
 
 
 console.log(JSON.stringify(movie_titles))
